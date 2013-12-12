01841ee263542746d7aa0ee53723f12028145ba0
obry <orby1@gmx.de>
2013-12-13 00:16:54 +0100

    redo

--- DIM_server_GC_final/Source.cpp
+++ DIM_server_GC_final/Source.cpp
@@ -30,6 +30,7 @@ string from_variant(VARIANT& vt)
 	_bstr_t bs(vt);
 	return std::string(static_cast<const char*>(bs));
 }
+
 int GetStreamNumber(string LatestFolderName)
 	// returns the Name of the Stream (Stream 1, 2, 3 ... 10) that was used by the Gaschromatograph's Stream-Selector,
 	// by reading "SampleName" from "Result.xml"
@@ -41,9 +42,10 @@ int GetStreamNumber(string LatestFolderName)
 		{
 			MSXML::IXMLDOMDocument2Ptr xmlDoc;
 			hr = xmlDoc.CreateInstance(__uuidof(MSXML::DOMDocument60), NULL, CLSCTX_INPROC_SERVER);
-			string tempLatestFolderName = LatestFolderName+"\\Result.xml";
+			string tempLatestFolderName = LatestFolderName+"/Result.xml";
 			wstring widestr = wstring(tempLatestFolderName.begin(), tempLatestFolderName.end());
 			const wchar_t * widecstr = widestr.c_str();
+			cout <<"tempLatestFolderName in GetStreamNumber() is =" <<tempLatestFolderName <<endl;
 			if (xmlDoc->load(widecstr) != VARIANT_TRUE)
 			{
 				printf("Unable to load Result.xml in function GetStreamNumber()\n");
@@ -72,6 +74,47 @@ int GetStreamNumber(string LatestFolderName)
 	}
 	return -1;
 }
+
+
+
+string GetIjnectionTime(string LatestFolderName)
+	// returns the InjectionDateTime
+	// by reading "SampleName" from "Result.xml"
+{
+	HRESULT hr = CoInitialize(NULL); 
+	if (SUCCEEDED(hr))
+	{
+		try
+		{
+			MSXML::IXMLDOMDocument2Ptr xmlDoc;
+			hr = xmlDoc.CreateInstance(__uuidof(MSXML::DOMDocument60), NULL, CLSCTX_INPROC_SERVER);
+			string tempLatestFolderName = LatestFolderName+"/Result.xml";
+			wstring widestr = wstring(tempLatestFolderName.begin(), tempLatestFolderName.end());
+			const wchar_t * widecstr = widestr.c_str();
+			if (xmlDoc->load(widecstr) != VARIANT_TRUE)
+			{
+				printf("Unable to load Result.xml in function GetIjnectionTime()\n");
+			}
+			else
+			{
+				xmlDoc->setProperty("SelectionLanguage", "XPath");
+				// Find out from which Stream of the Chromatograph's Streamselector the XML file was produced.
+				MSXML::IXMLDOMNodeListPtr SampleName = xmlDoc->getElementsByTagName("InjectionDateTime"); //Pointer to List of Elements with specified name
+				MSXML::IXMLDOMElementPtr spElementTemp = (MSXML::IXMLDOMElementPtr) SampleName->item[0];
+				MSXML::IXMLDOMTextPtr spText = spElementTemp->firstChild;
+				// spText->nodeValu is e.g. "Stream 1". Extract the integer Number of the Stream:
+				string InjectionTime = from_variant(spText->nodeValue);
+				return InjectionTime;
+			}
+		}
+		catch (_com_error &e)
+		{
+			printf("ERROR: %ws\n", e.ErrorMessage());
+		}
+		CoUninitialize();
+	}
+	return "-1";
+}
 int GetNumberOfPeaks(string LatestFolderName)
 	// returns the Number of Peaks that was found in "Result.xml"
 {
@@ -82,7 +125,7 @@ int GetNumberOfPeaks(string LatestFolderName)
 		{
 			MSXML::IXMLDOMDocument2Ptr xmlDoc;
 			hr = xmlDoc.CreateInstance(__uuidof(MSXML::DOMDocument60), NULL, CLSCTX_INPROC_SERVER);
-			string tempLatestFolderName = LatestFolderName+"\\Result.xml";
+			string tempLatestFolderName = LatestFolderName+"/Result.xml";
 			wstring widestr = wstring(tempLatestFolderName.begin(), tempLatestFolderName.end());
 			const wchar_t * widecstr = widestr.c_str();
 			if (xmlDoc->load(widecstr) != VARIANT_TRUE)
@@ -110,6 +153,10 @@ COMPLEXDATA GetPeakData(string LatestFolderName, _bstr_t ElementName)
 	//return Info with label 'ElementName' in "Result.xml" for all Peaks
 {
 	COMPLEXDATA Peaks;
+	for (int i=0;i<12;i++)
+	{
+		Peaks.farr[i]=-1; //fill with dummy data to be able to return something in any case.
+	}
 	HRESULT hr = CoInitialize(NULL); 
 	if (SUCCEEDED(hr))
 	{
@@ -123,7 +170,7 @@ COMPLEXDATA GetPeakData(string LatestFolderName, _bstr_t ElementName)
 			// I know that 'ffd.cFileName' where I start with this is already something like wchar_t
 			// But it works now, and I don't know yet how to combine two wchar_t like I need below etc.
 			// Who ever reads this. Forgive me :))
-			string tempLatestFolderName = LatestFolderName+"\\Result.xml";
+			string tempLatestFolderName = LatestFolderName+"/Result.xml";
 			wstring widestr = wstring(tempLatestFolderName.begin(), tempLatestFolderName.end());
 			const wchar_t * widecstr = widestr.c_str();
 			if (xmlDoc->load(widecstr) != VARIANT_TRUE)
@@ -138,8 +185,6 @@ COMPLEXDATA GetPeakData(string LatestFolderName, _bstr_t ElementName)
 
 				MSXML::IXMLDOMNodeListPtr PeakAreaPercent = xmlDoc->getElementsByTagName(ElementName); //Pointer to List of Elements with specified name
 				int NumberOfPeaks = PeakAreaPercent->length; //Number of Peaks should be equal to the number of AreaPercent entrys in XML File.
-				cout <<"NumberOfPeaks=" << NumberOfPeaks <<endl;
-
 				float * AreaPercentValue =new float[NumberOfPeaks];
 
 				MSXML::IXMLDOMElementPtr spElementTemp; //need this to access values in elements
@@ -164,7 +209,6 @@ COMPLEXDATA GetPeakData(string LatestFolderName, _bstr_t ElementName)
 		}
 		CoUninitialize();
 	}
-	cout << "GetPeakData() ran until second return" <<endl;
 	return Peaks;
 }
 
@@ -204,9 +248,9 @@ int _tmain(int argc, TCHAR *argv[])
 	_tprintf(TEXT("\nTarget directory is %s\n\n"), argv[1]);
 
 	// Prepare string for use with FindFile functions.  First, copy the
-	// string to a buffer, then append '\*' to the directory name.
+	// string to a buffer, then append '/*' to the directory name.
 	StringCchCopy(szDir, MAX_PATH, argv[1]);
-	StringCchCat(szDir, MAX_PATH, TEXT("\\*"));
+	StringCchCat(szDir, MAX_PATH, TEXT("/*"));
 
 	// Find the first file in the directory.
 	hFind = FindFirstFile(szDir, &ffd);
@@ -228,7 +272,7 @@ int _tmain(int argc, TCHAR *argv[])
 	// Convert userinput of Target Directory ( argv[1] ) to string in order to create full Path 'targetDirectory+"\\"+LatestFolderName' in the end.
 	string targetDirectory;
 	WideCharToMultiByte(CP_ACP,0,argv[1],-1, ch,260,&DefChar, NULL);
-	targetDirectory = string(ch);
+	targetDirectory = string(ch);	
 	//initialize some return values
 	int NumberOfPeaks;
 	int StreamNumber;
@@ -253,56 +297,76 @@ int _tmain(int argc, TCHAR *argv[])
 	COMPLEXDATA PeakAreaData;
 	COMPLEXDATA RTData;
 	DimService Stream1("Stream1_PeakAreas","F:7",(void *)&PeakAreaData, sizeof(PeakAreaData));
-	DimService tpcCO2Content("tpcCO2Content",CO2_TPC); 
-	DimService tpcArgonContent("tpcArgonContent",Argon_TPC); 
-	DimService tpcN2Content("tpcN2Content",N2_TPC); 
-	DimService tpcWaterContent("tpcWaterContent",Water_TPC);
-	DimServer::setDnsNode("localhost"); //tpc dim dns node: 'alitpcdimdns'
+	DimService tpcCO2Content("ALICE_GC.Actual.tpcCO2Content",CO2_TPC); 
+	DimService tpcArgonContent("ALICE_GC.Actual.tpcArgonContent",Argon_TPC); 
+	DimService tpcN2Content("ALICE_GC.Actual.tpcN2Content",N2_TPC); 
+	DimService tpcWaterContent("ALICE_GC.Actual.tpcWaterContent",Water_TPC);
+	DimServer::setDnsNode("alitpcdimdns"); //tpc dim dns node: 'alitpcdimdns'
 	DimServer::autoStartOff();//Instructs the server to wait for a new DimServer::start(name) in order to register newly declared services. Lets me use two DimServer::start() with independent DIM_DNS_NODE for TRD and TPC.
-	DimServer::start("TPC_GC");
-	
-	// TRD DIM server
-	DimServer::setDnsNode("localhost");
-	DimServer::start("TRD_GC");
-	DimServer::autoStartOn();
-	DimService StreamNumberService("StreamNumber",StreamNumber);
+	DimServer::start("ALICE_GC");
+
+	string InjectionTimeAndDate_store1;
+	string InjectionTimeAndDate_store2 = "this must not be empty" ;
 
 	while(1)
 	{
 		do
 		{
-			if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+			if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) //only act on folder changes
 			{
-				time1 = ffd.ftCreationTime;
-				if (firstRun) 
+				time1 = ffd.ftLastWriteTime;
+				if (firstRun) // only after startup of the program and only for the first folder found ...
 				{ 
 					time2 = time1;
 					firstRun = FALSE;
 				}
-				CompareFileTimeResult = CompareFileTime(&time1,&time2); //returns 1, 0 or -1 
-				if (CompareFileTimeResult == 1) // that is: First file time is later than second file time.
+				CompareFileTimeResult = CompareFileTime(&time1,&time2); //returns 1, 0 or -1
+				if ( CompareFileTimeResult == 1 ) // that is: First folder time is later than second folder time.
 				{
-					time2= time1;
+					time1 = ffd.ftLastWriteTime;
+					time2 = time1;
 					if (!firstCycle)
 					{
+						StreamNumber = -1; //avoid that DIM services will be updated if no file could be read.
 						WideCharToMultiByte(CP_ACP,0,ffd.cFileName,-1, ch,260,&DefChar, NULL);
-						LatestFolderName = targetDirectory+"\\"+string(ch);
-						NumberOfPeaks = GetNumberOfPeaks(LatestFolderName);
-						StreamNumber = GetStreamNumber(LatestFolderName);				
-						cout <<"Number of Peaks returned by GetNumberOfPeaks() = "<< NumberOfPeaks<<endl;
-						cout <<"StreamNumber returned by GetStreamNumber() = "<< StreamNumber<<endl;
-						PeakAreaData = GetPeakData(LatestFolderName, PeakArea);
-						RTData = GetPeakData(LatestFolderName, RT);
-						// decide based on Retention Time which peak is which and update DIM services accordingly:
-						for (int i=0;i<NumberOfPeaks;i++)
+						if ( string(ch) == "." )
 						{
-							if ( RTCO2_min<RTData.farr[i] && RTData.farr[i]<RTCO2_max ) CO2_TPC = PeakAreaData.farr[i], cout << "CO2 candidate: " << CO2_TPC <<endl , tpcCO2Content.updateService();
-							if ( RTArgon_min<RTData.farr[i] && RTData.farr[i]<RTArgon_max ) Argon_TPC = PeakAreaData.farr[i], cout << "Argon candidate: " << Argon_TPC <<endl , tpcArgonContent.updateService();
-							//cout << PeakArea <<" as COMPLEXDATA is : " << PeakAreaData.farr[i] << endl;
-							//cout << RT <<" as COMPLEXDATA is : " << RTData.farr[i] << endl;
+							cout << "WIN32_FIND_CHAR is seeing the parent folder change." <<endl;
 						}
-						Stream1.updateService(); 
-						StreamNumberService.updateService(); 
+						else 
+						{
+							cout << "string(ch) as used = " << string(ch) << endl;
+							LatestFolderName = targetDirectory+"/"+string(ch);
+							cout<< "sleep 20 s. Count down from 10 ;)" <<endl;
+							for (int i = 0; i<10;i++)
+							{
+								Sleep(2000);
+								cout << 10-i <<endl;
+							}
+							InjectionTimeAndDate_store1 = GetIjnectionTime(LatestFolderName);
+							cout << "InjectionTimeAndDate_store1: " << InjectionTimeAndDate_store1 <<endl;
+							NumberOfPeaks = GetNumberOfPeaks(LatestFolderName);
+							StreamNumber = GetStreamNumber(LatestFolderName);
+							PeakAreaData = GetPeakData(LatestFolderName, PeakArea);
+							RTData = GetPeakData(LatestFolderName, RT);
+							if (InjectionTimeAndDate_store1 != InjectionTimeAndDate_store2) //only update DIM Services if xml file that was read is indeed a new file.
+							{
+								InjectionTimeAndDate_store2 = InjectionTimeAndDate_store1;
+								for (int i=0;i<NumberOfPeaks;i++)
+								{
+									if ( StreamNumber == 1 ) // StreamNumber 1 is TPC GAS ==> update DIM Services for TPC
+									{
+										// decide based on Retention Time which peak is which and update DIM services accordingly:
+										if ( RTCO2_min<RTData.farr[i] && RTData.farr[i]<RTCO2_max ) CO2_TPC = PeakAreaData.farr[i], cout << "CO2 candidate: " << CO2_TPC <<endl , tpcCO2Content.updateService();
+										if ( RTArgon_min<RTData.farr[i] && RTData.farr[i]<RTArgon_max ) Argon_TPC = PeakAreaData.farr[i], cout << "Argon candidate: " << Argon_TPC <<endl , tpcArgonContent.updateService();
+									}
+									//cout << PeakArea <<" as COMPLEXDATA is : " << PeakAreaData.farr[i] << endl;
+									//cout << RT <<" as COMPLEXDATA is : " << RTData.farr[i] << endl;
+								}
+								if (PeakAreaData.farr[0] >= 0 ) Stream1.updateService(); //only update Stream1 when it contains real data (that is no dummy data "-1").
+
+							}
+						}
 					}
 				}
 			}
